8c7d1ffda4ad4699c3366dc349a482ad2ef0f411
agustinrabini <agustinrabini99@gmail.com>
2022-03-14 16:26:16 -0300

    first try for iniciate one client of frb

--- firebase.go
+++ firebase.go
@@ -6,39 +6,40 @@ import (
 	"strings"
 
 	firebase "firebase.google.com/go"
+	"firebase.google.com/go/auth"
 	"google.golang.org/api/option"
 )
 
-func AuthWithFirebase(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		opt := option.WithCredentialsFile("credentials.json")
-		app, err := firebase.NewApp(context.Background(), nil, opt)
-		if err != nil {
-			w.WriteHeader(500)
-			w.Write([]byte("Error on credentials " + err.Error()))
-			return
-		}
+func initFirebaseClient() *auth.Client {
 
-		auth, err2 := app.Auth(context.Background())
-		if err2 != nil {
-			w.WriteHeader(500)
-			w.Write([]byte("Error on credentials " + err2.Error()))
-			return
-		}
+	opt := option.WithCredentialsFile("credentials.json")
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+
+		return nil
+	}
+
+	auth, err2 := app.Auth(context.Background())
+	if err2 != nil {
+
+		return nil
+	}
+
+	return auth
+}
+
+func AuthWithFirebase(client *auth.Client) func(http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		header := r.Header.Get("HeaderAuthorization")
 		idToken := strings.TrimSpace(strings.Replace(header, "Bearer", "", 1))
-		_, err = auth.VerifyIDToken(context.Background(), idToken)
+		_, err := client.VerifyIDToken(context.Background(), idToken)
 		if err != nil {
-			w.WriteHeader(401)
-			w.Write([]byte("Error getting the token.\n" + err.Error()))
+
 			return
 		}
-
-		//compara uid
-		//crear cliente
-		//crear libreria
-
 		next.ServeHTTP(w, r)
 	})
+
 }
