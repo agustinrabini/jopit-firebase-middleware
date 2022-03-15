ed2c0848e38652d4a7a268fa05f1178fe52fb14c
agustinrabini <agustinrabini99@gmail.com>
2022-03-15 10:39:40 -0300

    second attempt

--- firebase.go
+++ firebase.go
@@ -2,39 +2,56 @@ package middleware
 
 import (
 	"context"
+	"log"
 	"net/http"
 	"strings"
+	"sync"
 
 	firebase "firebase.google.com/go"
 	"firebase.google.com/go/auth"
 	"google.golang.org/api/option"
 )
 
-func InitFirebaseClient(fileName string) *auth.Client {
+var (
+	firebaseClient *FirebaseClient
+	once           sync.Once
+)
 
-	opt := option.WithCredentialsFile(fileName)
+type FirebaseClient struct {
+	AuthClient *auth.Client
+}
+
+func Newfirebase() *FirebaseClient {
+	once.Do(InitFirebase)
+
+	return firebaseClient
+}
+
+func InitFirebase() {
+
+	opt := option.WithCredentialsFile("credentials.json")
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
-
-		return nil
+		log.Println("Error connecting to firebase" + err.Error())
 	}
 
 	auth, err2 := app.Auth(context.Background())
 	if err2 != nil {
-
-		return nil
+		log.Println("Error connecting to firebase" + err2.Error())
 	}
 
-	return auth
+	firebaseClient = &FirebaseClient{
+		AuthClient: auth,
+	}
 }
 
-func AuthWithFirebase(client *auth.Client) func(http.Handler) http.Handler {
+func AuthWithFirebase(next http.Handler) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 			header := r.Header.Get("HeaderAuthorization")
 			idToken := strings.TrimSpace(strings.Replace(header, "Bearer", "", 1))
-			_, err := client.VerifyIDToken(context.Background(), idToken)
+			_, err := firebaseClient.AuthClient.VerifyIDToken(context.Background(), idToken)
 			if err != nil {
 
 				return
@@ -43,3 +60,11 @@ func AuthWithFirebase(client *auth.Client) func(http.Handler) http.Handler {
 		})
 	}
 }
+
+/* func (f *FirebaseClient) Close() error {
+	if firebaseClient == nil {
+		return nil
+	}
+
+	return f
+} */
